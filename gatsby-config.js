fbe8b6a04a5639657f9529a333a083395b60ab98
Charlie <charlie@nazarios.net>
2024-06-05 21:48:53 -0400

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,10 @@
+/**
+ * @type {import('gatsby').GatsbyConfig}
+ */
+module.exports = {
+  siteMetadata: {
+    title: `Don the Con`,
+    siteUrl: `https://www.yourdomain.tld`,
+  },
+  plugins: [],
+}
